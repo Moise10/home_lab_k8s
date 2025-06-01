f80f39608c5b097a25196b9fc3691a9b7036711e
moise <mbakopmoise237@gmail.com>
2025-06-01 12:47:40 +0200

    setting up the backend

---
+++ terraform/backend/s3/main.tf
@@ -0,0 +1,21 @@
+resource "random_id" "bucket_suffix" {
+  byte_length = 8
+}
+
+resource "aws_s3_bucket" "s3_backend" {
+  bucket = "backend-s3-bucket-${random_id.bucket_suffix.hex}"
+  force_destroy = true
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+
+resource "aws_s3_bucket_versioning" "versioning_backend" {
+  bucket = aws_s3_bucket.s3_backend.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
